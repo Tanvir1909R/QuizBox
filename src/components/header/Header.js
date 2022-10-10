4874d23c8b8c4c2a5d54747d6f8a5f4020378a73
Tanvir1909R <trcr719@gmail.com>
2022-10-10 23:03:17 +0600

    header completed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './header.css'
 
 const Header = () => {
   return (
     <header>
         <div className="container">
             <div className="headerWrapper">
-                <div className="logo">Quiz<span>Box</span></div>
+                <div className="logo">Quiz <span>Box</span></div>
                 <nav>
                     <Link to='/'>Topics</Link>
                     <Link to='/statistics'>Statistics</Link>
