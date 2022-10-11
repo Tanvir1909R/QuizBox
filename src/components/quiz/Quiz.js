c8b19dfb98e6fc5ae72117bdb683b6f70cff9210
Tanvir1909R <trcr719@gmail.com>
2022-10-11 20:27:42 +0600

    add see correct ans

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Option from '../option/Option';
+import {AiFillEye, AiFillEyeInvisible} from 'react-icons/ai'
 import './quiz.css'
 
 const Quiz = ({question, setCorrect}) => {
+  const [visible, setVisible] = useState(false)
+  console.log(question);
     const { options } = question;
     const optionHandle = (e)=>{
       const targetDiv =  e.target.closest('.option');
@@ -13,13 +16,23 @@ const Quiz = ({question, setCorrect}) => {
         setCorrect(false);
       }
     }
+    const handleEye = ()=>{
+      setVisible(!visible)
+    }
   return (
     <div className='quiz'>
         <div className="question">
             <h1>{question.question}</h1>
+            {/* <p style={`${visible ? {opacity:'1'} : {opacity:'0'}}`}>{question.correctAnswer}</p> */}
+            {
+              visible ? <p>{question.correctAnswer}</p> : ''
+            }
         </div>
         <div className="options">
             {options.map((option, i) => <Option key={i} option={option} optionHandle={optionHandle}/>)}
+        </div>
+        <div className="eye" onClick={handleEye}>
+          { visible ? <AiFillEye /> : <AiFillEyeInvisible />}
         </div>
     </div>
   )
