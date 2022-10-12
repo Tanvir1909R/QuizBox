56bdd481f2aa795284e73e202350cb9084859e14
Tanvir1909R <trcr719@gmail.com>
2022-10-12 17:31:15 +0600

    edit readMeFile

--- src/components/option/Option.js
+++ src/components/option/Option.js
@@ -2,15 +2,6 @@ import React from 'react'
 import './option.css'
 
 const Option = ({option, toast}) => {
-  // const optionHandle = (e)=>{
-    //   const targetDiv =  e.target.closest('.option');
-    //   const correctAns = question.correctAnswer;
-    //   if(targetDiv.innerText === correctAns){
-    //     setCorrect(true);
-    //   }else{
-    //     setCorrect(false);
-    //   }
-    // }
 
   return (
     <div className='option' onClick={toast}>
